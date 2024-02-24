21d0971ce7c2460cfe0ae338734c1117fa0e4701
José Duque <duquejo01@gmail.com>
2024-02-23 21:13:52 -0500

    :green_heart: Adding Integration tests CI/CD

--- tests/integration/integration.spec.ts
+++ tests/integration/integration.spec.ts
@@ -1,16 +1,18 @@
-import { beforeAll, describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import request from 'supertest';
 
-describe('+ Integration tests', () => {
+const testingCondition = ((!!import.meta.env.VITE_CELEBRITIES_URL && !!import.meta.env.VITE_VOTES_URL) && (import.meta.env.MODE === 'test' || import.meta.env.PROD) );
 
-  let baseURL = '';
-  beforeAll(() => {
-    baseURL = import.meta.env.MODE === 'test' ? import.meta.env.VITE_CELEBRITIES_BASEURL : import.meta.env.VITE_CELEBRITIES_BASEURL;
-  });
+const describeConditionally = testingCondition ? describe : describe.skip;
+
+describeConditionally('+ Integration tests', () => {
+
+  const celebritiesApi: string = import.meta.env.VITE_CELEBRITIES_URL;
+  const votesApi: string = import.meta.env.VITE_VOTES_URL;
 
   it('Celebrities: /list (GET) - 200 Success', () => {
-    return request(baseURL)
-    .get('/list')
+    return request(celebritiesApi)
+    .get('')
     .set('Content-Type', 'application/json')
     .expect(200)
     .expect(res => {
@@ -19,8 +21,8 @@ describe('+ Integration tests', () => {
   });
 
   it('Votes: /save (POST) - 400 Empty body', () => {
-    return request(baseURL)
-    .post('/save')
+    return request(votesApi)
+    .post('')
     .set('Content-Type', 'application/json')
     .expect(400)
     .then(response => {
@@ -31,8 +33,8 @@ describe('+ Integration tests', () => {
   });
 
   it('Votes: /save (POST) - 400 The ID or vote parameter are missing', () => {
-    return request(baseURL)
-    .post('/save')
+    return request(votesApi)
+    .post('')
     .set('Content-Type', 'application/json')
     .send({ id: 5 })
     .expect(400)
@@ -44,8 +46,8 @@ describe('+ Integration tests', () => {
   });
 
   it('Votes: /save (POST) - 404 Celebrity not found', () => {
-    return request(baseURL)
-    .post('/save')
+    return request(votesApi)
+    .post('')
     .set('Content-Type', 'application/json')
     .send({ id: 123456789, vote: 'positive' })
     .expect(404)
@@ -57,8 +59,8 @@ describe('+ Integration tests', () => {
   });
 
   it('Votes: /save (POST) - The valid vote values are \'positive\' or \'negative\'', () => {
-    return request(baseURL)
-    .post('/save')
+    return request(votesApi)
+    .post('')
     .set('Content-Type', 'application/json')
     .send({ id: 5, vote: 'thing' })
     .expect(400)
@@ -70,8 +72,8 @@ describe('+ Integration tests', () => {
   });
 
   it('Votes: /save (POST) - 200 Success', () => {
-    return request(baseURL)
-    .post('/save')
+    return request(votesApi)
+    .post('')
     .set('Content-Type', 'application/json')
     .send({ id: 6, vote: 'positive' })
     .expect(200);
